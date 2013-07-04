070e10f94c2d2c3d89a456049ef418c8c295f171
Arturo Bernal <arturoberanlg@gmail.com>
2013-07-04 18:49:24 +0000

    homework3

--- web.js
+++ web.js
@@ -1,26 +1,36 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+/*
 var data="";
 fs = require('fs');
 
     fs.readFile("index.html", function (err, content) {
         if (err)
 		 return  -1
-	else
-	console.log("Ok" + content);
-	data=content;
+	
+	 data=content;
     })
 
-var buffer = new Buffer(data, "utf-8");
 app.get('/', function(request, response) {
 
+var te="Hola";
+ // response.send(buffer.toString('utf8', 0, buffer.length));
+*/
+app.get('/', function(request, response) {
 
-len = buffer.write('\u00bd + \u00bc = \u00be', 0);
-console.log(len + " bytes: " + buffer.toString('utf8', 0, len));
+var data="";
+fs = require('fs');
 
-  response.send(buffer.toString('utf8', 0, buffer.length));
- // response.send('Hello World!!!!');
+    fs.readFile("index.html", function (err, content) {
+        if (err)
+                 return  -1
+        
+         data=content;
+    })
+
+
+  response.send(data);
 
 });
 
