ad1b95d32e42e994d99e227d1f67ebdb08077de8
Arturo Bernal <arturoberanlg@gmail.com>
2013-07-04 13:06:40 +0000

    homework3

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var data="";
+fs = require('fs')
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
 
+var buffer = new Buffer(data, "utf-8")
 app.get('/', function(request, response) {
-  response.send('Hello World!!!!');
+
+  response.send(buffer.toString('utf8', 0, buffer.length));
+
 });
 
 var port = process.env.PORT || 5000;
