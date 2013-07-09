0483be8658d28b8779473bea6add4fd3e2001b8a
Arturo Bernal <arturoberanlg@gmail.com>
2013-07-09 10:56:21 +0000

    OK

---
+++ cherio.js
@@ -0,0 +1,18 @@
+var $ = require('cheerio')
+var request = require('../request')
+
+function gotHTML(err, resp, html) {
+  if (err) return console.error(err)
+  var parsedHTML = $.load(html)
+  // get all img tags and loop over them
+	console.log(parsedHTML);
+  var imageURLs = []
+  parsedHTML('a').map(function(i, link) {
+    var href = $(link).attr('href')
+    if (!href.match('.png')) return
+    imageURLs.push(domain + href)
+  })
+}
+
+var domain = 'http://substack.net/images/'
+request(domain, gotHTML)
---
+++ test.js
@@ -0,0 +1,17 @@
+var sys = require('util'),
+    rest = require('../restler');
+
+rest.get('http://google.com').on('complete', function(result) {
+  if (result instanceof Error) {
+    sys.puts('Error: ' + result.message);
+    this.retry(5000); // try again after 5 sec
+  } else {
+    sys.puts(result);
+  }
+});
+
+if(require.main == module) {
+
+console.log("Hola");
+}
+
