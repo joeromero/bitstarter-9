adf5e18904e740d4985127f5a0aeddcf590e150c
Arturo Bernal <arturoberanlg@gmail.com>
2013-07-18 09:30:14 +0000

    HW 4

--- web.js
+++ web.js
@@ -23,7 +23,7 @@ var buffer = new Buffer(data, "utf-8")
 
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
